8d88194574b857496ed63f861c750916019f7245
aniket <aniketavinashmore343@gmail.com>
2022-06-09 16:24:19 +0530

    fix: changes

--- config/configService.js
+++ config/configService.js
@@ -3,7 +3,7 @@ function createConfig() {
     MONGO_URI: process.env.MONGO_URI,
     PORT: process.env.PORT || "8080",
     NODE_ENV: process.env.NODE_ENV || "development",
-    SECRET_KEY: process.env.SECRET_KEY || "secret",
+    SECRET_KEY: process.env.SECRET_KEY || "secret_key",
     OUTLOOK_EMAIL: process.env.OUTLOOK_EMAIL,
     OUTLOOK_PASSWORD: process.env.OUTLOOK_PASSWORD,
   };
